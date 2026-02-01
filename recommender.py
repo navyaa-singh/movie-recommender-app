76036ce2c108e65f7e08e0d4945a57d86d661ded
navyaa-singh <navya.singh.0302@gmail.com>
2026-02-02 02:22:45 +0530

    Update recommender.py

--- recommender.py
+++ recommender.py
@@ -5,7 +5,7 @@ import gdown  # This helper library handles the download
 
 # 1. Your Google Drive File ID
 file_id = '1-gmcKrv_9FYmu7P9NxnXEla1GWwGF3m1' 
-url = f'https://drive.google.com/uc?id={1-gmcKrv_9FYmu7P9NxnXEla1GWwGF3m1}'
+url = 'https://drive.google.com/uc?id=1-gmcKrv_9FYmu7P9NxnXEla1GWwGF3m1'
 output = 'similarity.pbz2'
 
 # 2. This checks if the file is already there; if not, it downloads it
@@ -16,5 +16,6 @@ if not os.path.exists(output):
 def load_similarity():
     with bz2.BZ2File(output, 'rb') as f:
         return pickle.load(f)
+
 
 similarity = load_similarity()
