0aac396a2acd53c48e9b2f6884560d154eb01887
navyaa-singh <navya.singh.0302@gmail.com>
2026-02-02 02:59:09 +0530

    Enhance movie selection with dropdown menu
    Replaced text input with a dropdown for movie selection and added movie list loading.

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
 import streamlit as st
 from recommender import recommend
+import pickle
+
+# Load the movie list for the dropdown
+movies = pickle.load(open('movies.pkl', 'rb'))
 
 st.title("🎬 My Movie Recommender App")
 
-movie_name = st.text_input("Enter a movie name")
+# This creates a searchable dropdown menu
+selected_movie = st.selectbox(
+    "Type or select a movie from the list",
+    movies['title'].values
+)
 
-if st.button("Recommend"):
-    results = recommend(movie_name)
-
-    if results:
-        st.subheader("Recommended Movies:")
-        for movie in results:
-            st.write("👉", movie)
-    else:
-        st.write("Please enter a movie name.")
+if st.button('Recommend'):
+    results = recommend(selected_movie)
+    st.write("### Recommended Movies:")
+    for i in results:
+        st.write(f"👉 {i}")
